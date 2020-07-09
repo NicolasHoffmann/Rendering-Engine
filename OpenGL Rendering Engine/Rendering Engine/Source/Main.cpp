4f7840c57a3b98654fc235df0b9aefea46340d22
Nicolas Hoffmann - PC Acer <nicolas.hoffmann@stud.h-da.de>
2020-07-09 20:49:35 +0200

    Added first Shader (NOT WORKING)
     - Deutsche Kommentare
     - Shader eingefügt
     - Dreieck erstellt
     - WICHTIG: CODE FUNKTIONIERT NCIHT

--- OpenGL Rendering Engine/Rendering Engine/Source/Main.cpp
+++ OpenGL Rendering Engine/Rendering Engine/Source/Main.cpp
@@ -1,52 +1,80 @@
-//GLFW - Documentation. (o. J.). Abgerufen 9. Juli 2020, von https://www.glfw.org/documentation.html
-//GLEW: The OpenGL Extension Wrangler Library. (o. J.). Abgerufen 9. Juli 2020, von http://glew.sourceforge.net/basic.html
+// GLFW - Documentation. (o. J.). Abgerufen 9. Juli 2020, von https://www.glfw.org/documentation.html
+// GLEW: The OpenGL Extension Wrangler Library. (o. J.). Abgerufen 9. Juli 2020, von http://glew.sourceforge.net/basic.html
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
+// Include
+	#include <GL/glew.h>
+	#include <GLFW/glfw3.h>
 
-int main(void)
-{
+	#include <iostream>
+
+	#include "ShaderUtil.h"
+
+
+
+int main(void) {
 	GLFWwindow* window;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+	// Bibliothek wird initialisiert
+		if (!glfwInit())
+			return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
-	/* width, height, name*/
-	window = glfwCreateWindow(640, 480, "Rendering Engine", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
+	// Fenster mit OpenGL-Kontext wird generiert
+		// Breite, Höhe, Name
+			window = glfwCreateWindow(640, 480, "Rendering Engine", NULL, NULL);
+			if (!window) {
+				glfwTerminate();
+				return -1;
+			}
 
-	/* Make the window's context current */
+
 	glfwMakeContextCurrent(window);
 
 	GLenum err = glewInit();
-	if (GLEW_OK != err)
-	{
-		/* Problem: glewInit failed, something is seriously wrong. */
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+
+	if (GLEW_OK != err) {
+		// Es gab einen Fehler mit glewInit(), der ausgegeben wird
+			fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
 	}
-	fprintf(stdout, "GLEW %s wird verwendet \n", glewGetString(GLEW_VERSION));
 
+	else {
+		// GLEW Version wird auf der Konsole ausgegeben
+			fprintf(stdout, "GLEW %s wird verwendet \n", glewGetString(GLEW_VERSION));
 
+		ShaderUtil shaderUtil;
+		shaderUtil.Load("shaders/vs.shader", "shaders/fs.shader");
 
+		// Eckpunkte des Dreiecks als Float
+			float points[6] = {
+				-0.8f, -0.5f,
+				0.0f, 0.9f,
+				0.5f, -0.7f
+			};
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		unsigned int buffer;
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
+		glGenBuffers(1, &buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), points, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-		/* Poll for and process events */
-		glfwPollEvents();
+		// Ein Loop der durch Schließen des fensters beendet wird
+			while (!glfwWindowShouldClose(window)) {
+
+				// Hier wird gerendert
+					glClear(GL_COLOR_BUFFER_BIT);
+
+					glDrawArrays(GL_TRIANGLES, 0, 3);
+
+				// Buffer Swap: Front- und Back-Imagebuffer werden ausgetauscht
+					glfwSwapBuffers(window);
+
+				// Ereignisse werden abgestimmt und verarbeitet
+					glfwPollEvents();
+			}
+
+			shaderUtil.Delete();
+
 	}
 
 	glfwTerminate();
---
+++ OpenGL Rendering Engine/Rendering Engine/shaders/fs.shader
@@ -0,0 +1,11 @@
+// Jayanam Games. (2018, Mai 5). OpenGL C++ Tutorial : Shaders. Abgerufen 9. Juli 2020, von https://www.patreon.com/posts/opengl-c-shaders-17380566
+
+
+#version 400
+
+out vec4 color;
+
+void main ()  
+{     
+   color = vec4 (0.5, 0.0, 1.0, 1.0);  
+}

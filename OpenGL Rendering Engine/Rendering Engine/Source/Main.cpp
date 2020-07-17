d1568db00aba6fe6db6945232de7f7d4fd47d854
Nicolas Hoffmann - PC Acer <nicolas.hoffmann@stud.h-da.de>
2020-07-17 18:29:42 +0200

    debugging Function
     - Funktion GLTest wurde eingebaut um den OpenGL-Code auf Fehler zu prüfen

--- OpenGL Rendering Engine/Rendering Engine/Source/Main.cpp
+++ OpenGL Rendering Engine/Rendering Engine/Source/Main.cpp
@@ -1,5 +1,6 @@
 // GLFW - Documentation. (o. J.). Abgerufen 9. Juli 2020, von https://www.glfw.org/documentation.html
 // GLEW: The OpenGL Extension Wrangler Library. (o. J.). Abgerufen 9. Juli 2020, von http://glew.sourceforge.net/basic.html
+// The Cherno. (2020, Februar 12). OpenGL [Video-Datei]. Abgerufen von https://www.youtube.com/playlist?list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2
 
 // Include
 	#include <GL/glew.h>
@@ -10,6 +11,27 @@
 	#include <string>
 	#include <sstream>
 
+	#define ASSERT(x) if (!(x)) __debugbreak();
+	#define GLTest(x) glClearError();\
+		x;\
+		ASSERT(glLogCall(#x, __FILE__, __LINE__))
+
+
+static void glClearError() {
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool glLogCall(const char* function, const char* file, int line) {
+	while (GLenum error = glGetError()) {
+		std::cout << std::endl 
+			<< "[OpenGL Fehler] (" << error << ") " << std::endl 
+			<< "Funktion: "<< function << std::endl 
+			<< "Datei: " << file << std::endl 
+			<< "Zeile: " << line << std::endl;
+		return false;
+	}
+	return true;
+}
 
 struct ShaderProgramSource {
 	std::string vertexShaderSource;
@@ -78,24 +100,24 @@ static ShaderProgramSource ParseShader(const std::string& vertexFilepath, const 
 	static unsigned int CompileShader(unsigned int type, const std::string& source) {
 		unsigned int id = glCreateShader(type);
 		const char* src = source.c_str(); // gibt die Speicher-Adresse des ersten String-Zeichens zurück
-		glShaderSource(id, 1, &src, nullptr); // die Quelle des Shaders wird festgelegt (um welchen Shader geht es (id) , Anzahl der Quellcodes , Pointer die auf den String deuten die den Quellcode für den Shader beinhalten , String längen werden definiert)
-		glCompileShader(id); // Shader mit bestimmter ID wird kompiliert
+		GLTest(glShaderSource(id, 1, &src, nullptr)); // die Quelle des Shaders wird festgelegt (um welchen Shader geht es (id) , Anzahl der Quellcodes , Pointer die auf den String deuten die den Quellcode für den Shader beinhalten , String längen werden definiert)
+		GLTest(glCompileShader(id)); // Shader mit bestimmter ID wird kompiliert
 
 		int status;
-		glGetShaderiv(id, GL_COMPILE_STATUS, &status); // gibt einen Parameter aus einem Shader zurück (ID des Shaders , Parameter-Name (hier: der Status der Kompilierung) , Speicher-Adresse für den angefragten Parameter)
+		GLTest(glGetShaderiv(id, GL_COMPILE_STATUS, &status)); // gibt einen Parameter aus einem Shader zurück (ID des Shaders , Parameter-Name (hier: der Status der Kompilierung) , Speicher-Adresse für den angefragten Parameter)
 
 		// wenn es es ein Problem beim Kompilieren gab
 			if (status == GL_FALSE) {
 				int length;
-				glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+				GLTest(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
 				char* message = (char*)alloca(length * sizeof(char)); // Variable für die Message wird mit abgefragter Länge des Info-Log erstellt
 
-				glGetShaderInfoLog(id, length, &length, message); // gibt das Info Log für einen Shader zurück (ID des Shaders , größe des Char Buffers in den das Info Log gespeichert wird , Speicher-Adresse der Länge, Zwischenspeicher (Buffer) für die Message)
+				GLTest(glGetShaderInfoLog(id, length, &length, message)); // gibt das Info Log für einen Shader zurück (ID des Shaders , größe des Char Buffers in den das Info Log gespeichert wird , Speicher-Adresse der Länge, Zwischenspeicher (Buffer) für die Message)
 				
 				std::cout << "Kompilierungsfehler: " << std::endl;
 				std::cout << message << std::endl; // Message wird auf der Konsole ausgegeben
 
-				glDeleteShader(id); // da der Shader nicht kompiliert werden konnte, wird er gelöscht
+				GLTest(glDeleteShader(id)); // da der Shader nicht kompiliert werden konnte, wird er gelöscht
 
 				return 0;
 			}
@@ -110,14 +132,14 @@ static ShaderProgramSource ParseShader(const std::string& vertexFilepath, const 
 		unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader); // ruft die CompileShader-Methode (oben) auf
 
 		// Shader-Programm wird erstellt
-			glAttachShader(program, vs); // Shader wird zum Programm-Objekt hinzugefügt
-			glAttachShader(program, fs); // Shader wird zum Programm-Objekt hinzugefügt
-			glLinkProgram(program); // Das Programm wird zusammengefügt und die angehängten Shader werden zu Executables, die auf dem jeweiligen Prozessor laufen
-			glValidateProgram(program); // es wird geprüft, ob sich die Executables des Programms ausfüren lassen
+			GLTest(glAttachShader(program, vs)); // Shader wird zum Programm-Objekt hinzugefügt
+			GLTest(glAttachShader(program, fs)); // Shader wird zum Programm-Objekt hinzugefügt
+			GLTest(glLinkProgram(program)); // Das Programm wird zusammengefügt und die angehängten Shader werden zu Executables, die auf dem jeweiligen Prozessor laufen
+			GLTest(glValidateProgram(program)); // es wird geprüft, ob sich die Executables des Programms ausfüren lassen
 
 		// Shader können gelöscht werden, da sie nun Teil eines Programms sind
-			glDeleteShader(vs);
-			glDeleteShader(fs);
+			GLTest(glDeleteShader(vs));
+			GLTest(glDeleteShader(fs));
 
 		return program;
 	}
@@ -186,25 +208,25 @@ int main(void) {
 
 		// Vertex Array Objekt
 			unsigned int vertexArrayObject;
-			glGenVertexArrays(1, &vertexArrayObject);
-			glBindVertexArray(vertexArrayObject);
+			GLTest(glGenVertexArrays(1, &vertexArrayObject));
+			GLTest(glBindVertexArray(vertexArrayObject));
 
 		// Buffer (Zwischenspeicher)
 			// Vertex-Buffer
 				unsigned int vertexBuffer;
-				glGenBuffers(1, &vertexBuffer); // Generiert eine Anzahl von Buffern und liefert die Adresse als ID zurück
+				GLTest(glGenBuffers(1, &vertexBuffer)); // Generiert eine Anzahl von Buffern und liefert die Adresse als ID zurück
 
-				glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer); // Wählt den aktuell zu benutzenden Buffer aus
-					glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), points, GL_STATIC_DRAW); // gibt dem (Array-)Buffer eine festgelegte Anzahl an Bytes und füllt ihn mit Daten (NULL = nicht füllen)
-					glEnableVertexAttribArray(0); // Ein Vertex-Attribut mit einem bestimmten Index (hier: 0) wird aktiviert
-					glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0); // (Index des Attributs (hier: Koordinaten)(Index: 0) ,  wie viele Attribute des Datentyps (hier: float) hat der Vertex (hier: 2) , Datentyp , ist der Inhalt normalisiert , wie viele Byte lang ist ein Vertex (hier: 2 * float-Länge) , am wievielten Byte im Vertex fängt das Attribut an (hier: 0));
+				GLTest(glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer)); // Wählt den aktuell zu benutzenden Buffer aus
+					GLTest(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), points, GL_STATIC_DRAW)); // gibt dem (Array-)Buffer eine festgelegte Anzahl an Bytes und füllt ihn mit Daten (NULL = nicht füllen)
+					GLTest(glEnableVertexAttribArray(0)); // Ein Vertex-Attribut mit einem bestimmten Index (hier: 0) wird aktiviert
+					GLTest(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0)); // (Index des Attributs (hier: Koordinaten)(Index: 0) ,  wie viele Attribute des Datentyps (hier: float) hat der Vertex (hier: 2) , Datentyp , ist der Inhalt normalisiert , wie viele Byte lang ist ein Vertex (hier: 2 * float-Länge) , am wievielten Byte im Vertex fängt das Attribut an (hier: 0));
 
 			// Index-Buffer
 				unsigned int indexBuffer;
-				glGenBuffers(1, &indexBuffer);
+				GLTest(glGenBuffers(1, &indexBuffer));
 
-				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-					glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(unsigned int), index, GL_STATIC_DRAW);
+				GLTest(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer));
+				GLTest(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(unsigned int), index, GL_STATIC_DRAW));
 
 		// Shader Programm erstellen
 			ShaderProgramSource source = ParseShader(
@@ -213,7 +235,7 @@ int main(void) {
 			);
 
 			unsigned int shaderProgram = CreateShaderProgram(source.vertexShaderSource, source.fragmentShaderSource);
-			glUseProgram(shaderProgram);
+			GLTest(glUseProgram(shaderProgram));
 
 		// Farbe festlegen
 			float r = 0.0f;
@@ -225,36 +247,39 @@ int main(void) {
 		// Ein Loop der durch Schließen des fensters beendet wird
 			while (!glfwWindowShouldClose(window)) {
 
-				if (forward == true) {
-					r += 0.05f;
+				// Farbe verändert sich
+					if (forward == true) {
+						r += 0.05f;
 
-					if (r >= 0.99f) {
-						forward = false;
-					}	
-				}
-				else if (forward == false) {
-					r -= 0.05f;
-
-					if (r <= 0.01f) {
-						forward = true;
+						if (r >= 0.99f) {
+							forward = false;
+						}	
 					}
-				}
+					else if (forward == false) {
+						r -= 0.05f;
+
+						if (r <= 0.01f) {
+							forward = true;
+						}
+					}
 
 
-				std::cout << r << std::endl;
+				// std::cout << r << std::endl;
 
 				// Hier wird gerendert
 					glClear(GL_COLOR_BUFFER_BIT);
+					glClearError();
 
-					glUseProgram(shaderProgram);
+					GLTest(glUseProgram(shaderProgram));
 
 					// neue Farbe wird übergeben
-						glUniform4f(location, r, 1.0f, 0.7f, 1.0f);
+						GLTest(glUniform4f(location, r, 1.0f, 0.7f, 1.0f));
 
-						glBindVertexArray(vertexArrayObject);
-						glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+						GLTest(glBindVertexArray(vertexArrayObject));
+						GLTest(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer));
 
-					glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+					// Elemente werden gezeichnet
+						GLTest(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 				// Buffer Swap: Front- und Back-Imagebuffer werden ausgetauscht
 					glfwSwapBuffers(window);
@@ -262,7 +287,7 @@ int main(void) {
 				// Ereignisse werden abgestimmt und verarbeitet
 					glfwPollEvents();
 			}
-			glDeleteProgram(shaderProgram);
+			GLTest(glDeleteProgram(shaderProgram));
 
 	}
 
